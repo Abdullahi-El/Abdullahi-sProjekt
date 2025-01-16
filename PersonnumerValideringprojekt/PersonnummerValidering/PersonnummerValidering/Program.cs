20b56b1cb6d5e05f7b141f817ed57cb56e81f966
Abdullahi-El <91474775+Abdullahi-El@users.noreply.github.com>
2025-01-16 18:09:04 +0100

    Add files via upload

---
+++ PersonnumerValideringprojekt/PersonnummerValidering/PersonnummerValidering/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Välkommen till Personnummerkontrollen!");
+        Console.WriteLine("Skriv in ett svenskt personnummer (10 eller 12 siffror):");
+
+        string personnummer = Console.ReadLine();
+
+        if (PersonnummerValidering.PersonnummerValidator.ValideraPersonnummer(personnummer))
+        {
+            Console.WriteLine("Personnumret är giltigt!");
+        }
+        else
+        {
+            Console.WriteLine("Personnumret är ogiltigt!");
+        }
+    }
+}
